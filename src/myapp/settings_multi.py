0aaa474328d59ab3443ba4b4690312571af87ade
kenta azuma <feiz@feiMac3.local>
2015-12-24 16:19:04 +0900

    passwordを記載

--- src/myapp/settings_multi.py
+++ src/myapp/settings_multi.py
@@ -5,6 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myapp_db',
         'USER': 'www',
+        'PASSWORD': 'testpassword',
         'HOST': 'db',
     },
 }
--- src/myapp/settings_single.py
+++ src/myapp/settings_single.py
@@ -5,6 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myapp_db',
         'USER': 'www',
+        'PASSWORD': 'testpassword',
         'HOST': '127.0.0.1',
     },
 }
